c130344b4ef7cab1bc7a32dfb992bdccfb9a9589
rsmith-github <rsmith.git@gmail.com>
2022-08-03 18:30:12 +0100

    player moving

--- index.js
+++ index.js
@@ -25,27 +25,44 @@ function CreatePlayer() {
     player.style.border = "solid 1px black";
     player.style.height = "15px";
     player.style.width = "150px";
+    player.style.marginBottom = "50px";
+    player.style.marginLeft = "338px";
     player.style.backgroundColor = "#98B4D4";
     player.style.float = "bottom"
+
+    player.style.position = "relative";
+    player.style.animationName = "move";
+    player.style.animationDuration = "2s";
+    player.style.animationFillMode = "forwards";
+
 
     document.querySelector("#game-container").appendChild(player);
 }
 
 CreatePlayer();
+let margin = 338
+function MovePlayer(event) {
 
-let margin = 5;
-function MovePlayer() {
-    document.onkeydown = function (event) {
-        switch (event.key) {
-            case "ArrowLeft":
-                break;
-            case "ArrowRight":
-                player.style.marginLeft += margin + "px";
-                margin += 5
-                // break;
 
-        }
-    };
+    switch (event.key) {
+        case "ArrowLeft":
+            margin -= 10;
+            player.style.marginLeft = margin + "px";
+            if (margin <= 0) {
+                margin += 10;
+            }
+            break;
+        case "ArrowRight":
+            margin += 10;
+            player.style.marginLeft = margin + "px";
+            if (margin >= 698) {
+                margin -= 10;
+            }
+            break;
+
+    }
+
 }
 
-MovePlayer();
+document.addEventListener('keydown', MovePlayer);
+
