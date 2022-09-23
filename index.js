866b1377c20437fcf20bf17987c45b8a542d61fc
rsmith-github <rsmith.git@gmail.com>
2022-09-23 21:43:30 +0100

    fix: attempt to fix double tap zoom issue on iOS

--- index.js
+++ index.js
@@ -121,8 +121,22 @@ leftbtn.addEventListener("touchstart", MoveLeft);
 rightbtn.addEventListener("touchstart", MoveRight);
 
 
+function preventZoom(e) {
+    var t2 = e.timeStamp;
+    var t1 = e.currentTarget.dataset.lastTouch || t2;
+    var dt = t2 - t1;
+    var fingers = e.touches.length;
+    e.currentTarget.dataset.lastTouch = t2;
+
+    if (!dt || dt > 500 || fingers > 1) return; // not double-tap
+
+    e.preventDefault();
+    e.target.click();
+}
+
 // Mobile move left
-function MoveLeft() {
+function MoveLeft(e) {
+    preventZoom(e);
     if (!gameStart) {
         gameStart = true;
         Game();
@@ -138,7 +152,8 @@ function MoveLeft() {
 }
 
 // Mobile move right
-function MoveRight() {
+function MoveRight(e) {
+    preventZoom(e);
     if (!gameStart) {
         gameStart = true
         Game();
